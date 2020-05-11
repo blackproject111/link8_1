860e5727ff6d1cb0336b030d8f62fc1eb5a3e97e
blackproject111 <65025192+blackproject111@users.noreply.github.com>
2020-05-11 15:13:07 +0300

    1
    1

---
+++ about.php
@@ -0,0 +1,153 @@
+<?php
+$date = date('d-m-Y', time()-rand(13324, 1622211));
+$likes = rand(0, 100);
+$views = rand(0, 1000);
+?>
+
+<div class="col-lg-1 col-md-0"></div>
+<div class="col-lg-10 col-md-12">
+
+    <div class="main-post">
+
+        <div class="post-top-area">
+
+            <h5 class="pre-title">Blog tentang kecantikan dan kesehatan!</h5>
+
+            <h3 class="title"><a href="#"><b id="title"></b></a></h3>
+
+            <div class="post-info">
+
+                <div class="left-area">
+                    <a class="avatar" href="#"><img src="img/background/0843-Oa-ahaABi6w.jpg" alt="Blog tentang kecantikan dan kesehatan!"></a>
+                </div>
+
+                <div class="middle-area">
+                    <a class="name" href="#"><b>tentang keindahan</b></a>
+                    <h6 class="date"><?=$date?></h6>
+                </div>
+
+            </div><!-- post-info -->
+            <br>
+            <img class="w-100" src="./img/themes/healthandbeauty/sarah-dorweiler-9Z1KRIfpBTM-unsplash.jpg" alt="">
+            <p class="para"> Topic <strong>: Peach untuk wajah. Masker wajah persik</strong>
+
+				<div class="entry-content">
+			<p>masker wajah adalah produk kosmetik yang efektif yang membuat kulit dalam keadaan muda untuk jangka waktu yang lama. Menerapkan mereka secara teratur di rumah, Anda dapat mencapai hasil yang luar biasa. Mereka bisa meremajakan wajah, halus keluar keriput, dan memperkaya kulit epidermis dengan vitamin yang penting, makro-dan mikro. Buah musim panas juicy adalah terutama berguna untuk matang, kulit memudar.</p>
+<h2><gaya span= " color: # 33966;" > Peach untuk wajah: tunjangan, properti</span></h2>
+<p>masker peach berguna karena mereka memiliki efek pelembab yang kuat. Ini penting untuk semua jenis kulit. Jika obat alam diterapkan secara teratur, dalam kursus, kulit akan mendapatkan tampilan velvety, kelenturan dan elastisitas akan dipulihkan, warna akan meningkat, kelelahan tanda, jerawat akan pergi, dan keriput akan jauh lebih sedikit. Penggunaan peach untuk wajah karena komposisi unik dari buah. Dalam dirinya mengandung banyak zat aktif bahwa ketika diterapkan menembus ke dalam lapisan dalam dari dermis, memberikan efek penyembuhan.</p>
+<p>komposisi persik termasuk:</p>
+<p > keriput E, yang mendukung pemuda sel, perkelahian, mencegah penampilan mereka, memiliki efek angkat.<br / >
+Vitamin a menyelesaikan ruam, ia menghilangkan zat berbahaya, mempertahankan keseimbangan air normal, dan memperlambat proses layu.<br / >
+Vitamin B sedikit memutihkan kulit.<br / >
+Vitamin P memperkuat dinding pembuluh darah.<br / >
+Beta-Karotene mendukung kulit dalam keadaan muda, mempercepat regenerasi sel.<br / >
+Asam lemak bertarung dengan kekeringan di permukaan, memperkaya zat yang berguna yang memiliki exfoliating dan meremajakan efek.<br / >
+Vitamin C adalah antioksidant yang memulihkan turgor.<br / >
+Kalium ambil bagian dalam hidrasi.<br / >
+Vitamin B9 memiliki sifat pelindung dan menolak pengaruh negatif faktor lingkungan.<br / >
+Silikon memiliki efek restoratif aksi setelah peradangan, retak, perkelahian kulit kendur, mengaktifkan sintesis dari kolagen dan elastin.</p>
+<p>masker Peach mengandung banyak zat yang berguna yang mendukung proses-proses vital yang terjadi pada tingkat sel.</p>
+<h3><gaya span= " color: # 33966;" > Peach dalam kosmetologi</span></h3>
+<p>Peach telah banyak digunakan di kosmetik. Topeng dengan itu dapat digunakan oleh wanita dari segala usia, dengan jenis kulit yang berbeda. Mereka mampu memecahkan banyak masalah kosmetik yang muncul selama hidup, yaitu:</p>
+<p>jika kulit berminyak dengan sejumlah besar jerawat, mereka akan membantu menormalkan sekresi sebum, menghilangkan peradangan, dan menyingkirkan gatal.<br />
+Kering & # 8212; akan memperkaya dengan nutrisi, menyingkirkan keketasan,mengupas.<br / >
+Ketika kusam & # 8212; akan membuat kulit segar, sehat, halus.<br / >
+Jika kulit lembek, dengan banyak deep & #8212; nada keriput, mengembalikan elastisitas, meningkatkan konturansi Wajah oval.<br / >
+Buah bisa gunakan untuk meremajakan kulit sensitif rentan untuk iritasi, kekeringan,merah.</p>
+<h3><span style= "color: #33966;" >fitur aplikasi</span></h3>
+<UI>
+<li>untuk mempersiapkan masker, Anda perlu memilih segar, buah matang, tanpa penyok atau kerusakan.</li>
+<li>anda dapat menggunakan kedua pulp dan jus persik.</li>
+alat persiapan <li> segera sebelum memulai prosedur.</li>
+<li>masker dengan hati-hati di aduk untuk menghindari benjolan.</li>
+<li>Terapkan mereka di arah garis pijat.</li>
+<li>mendaftar dua kali seminggu, tahan selama 20-25 menit.</li>
+<li>cuci dengan air, atau decoction herbal.</li>
+<li>kemudian menerapkan agen bergizi.</li>
+</ul>
+warna <h3><span=": # 3396; " >Contractures</span></h3>
+<p>Individual intoleransi komponen.</p>
+<h2><gaya span= "color: # 33966;" >wajah wajah persik: resep rumah</span></h2>
+<h3><span style= "color: # 33966;" >peach Mask untuk kulit normal</span></h3>
+<p>ke melembabkan kulit wajah normal, memberikan naungan matte, persik berukuran sedang harus dimasak selama 25-30 menit., kemudian mash dengan garpu ke puree tebal, bercampur dengan satu sendok makan madu cair,dan porsi yang sama oatmeal. Sebarkan campuran campuran pada area masalah dan jaga selama 15-20 menit.  Cuci masker dengan air pada suhu kamar, atau decoction chamomile. Untuk mencapai hasil positif, melakukan prosedur hingga dua kali sehari seminggu, selama satu bulan.</p>
+<h3><span style= "color: # 33966;" >Peach mask untuk kulit kering</span></h3>
+<p>Resep digunakan sebagai agen anti-penuaan yang efektif. Untuk memberikan wajah kesegaran, velvety, satu buah matang dihancurkan pada sebuah parutan halus, digosok ke puree, dicampur dengan satu kuning telur dan satu tsp. liquid madu. Campuran ini didistribusikan pada dermis arah garis pijat, berbaring untuk beristirahat selama 25 menit. setelah waktu berlalu, mencuci dengan air, kemudian menerapkan krim bergizi.</p>
+<h3><span style= "color: # 33966;" >Peach mask untuk kulit berminyak</span></h3>
+<p>kupas persik, tepatkan atau gunakan blender sampai halus massa, menggabungkan dengan dua sendok lemon jus, sebagian kecil dari oatmeal, aduk semuanya, menerapkan isi untuk epidermis di lapisan seragam, tetap 25-30 menit, setelah suatu waktu, mandi dengan air pada suhu kamar.</p>
+<p>2. Setengah juicy peach mash sampai halus, menggabungkan dengan satu tsp kentang starch, campuran mendistribusikan pada permukaan masalah, memungkinkan waktu untuk menyerap zat yang berguna, setelah 15-20 menit. cuci sisanya dengan air yang mengalir atau rebusan herbal.</p>
+<h3><span style= "color: # 33966;" >peach Anti-Kerut Masker Wajah</span></h3>
+<p>membersihkan persik, gosok ke puree, mengambil dua sendok makan massa, menggabungkan dengan satu sendok minyak almond (atau zaitun), terapkan campuran untuk epidermis sepanjang garis pijat, tinggalkan untuk menyerap selama 25 menit, setelah waktu berlalu, bersihkan sisa-sisa dengan air berjalan pada suhu kamar, kemudian bersihkan wajah dengan batu es.</p>
+<p>2. Kalahkan satu kuning telur ke froth, campuran dengan dua tbsp oatmeal, satu tsp minyak zaitun, tambahkan massa buah, aduk semuanya, berlaku untuk kulit, menunggu 20-25 min., mencuci sisa-sisa dengan rebusan herbal, kemudian menerapkan krim bergizi.</p>
+<h3><span style= "color: # 33966;" >peach Mask untuk kulit memudar </span></h3>
+<p>RUB satu telur yolk baik, dikombinasikan dengan satu tsp krim Media-lemak, menambahkan massa persik, semua campur bahan-bahan, menerapkan lapisan tebal masker pada kulit sepanjang garis pijat, tetap selama 15-20 menit.setelah waktu berlalu, cuci dengan air pada suhu kamar. Selesaikan prosedur dengan menerapkan tonik pelembab.</p>
+<p>2. Bilas persik yang matang dengan air, menghapus kulit, menyobek daging dengan parutan halus, atau memotongnya menjadi dalam blender,menggabungkan dengan satu tsp yogurt buatan sendiri, satu telur putih dikocok ke buih. Semua komponen dicampur sampai halus, menerapkan masker ke daerah masalah, berbaring untuk beristirahat selama setengah jam, kemudian menghapus sisa-sisa dengan spons basah. Bilas wajahmu dengan rebusan herbal.</p>
+<h3><span style= "warna: #33966;" >menenangkan masker wajah sensitif kulit</span></h3>
+<p>melebur peach pulp dengan jumlah kecil infus chamomile, tambah oatmeal untuk kepadatan. Kenakan masker ke kulit, tahan selama 15 menit, kemudian bilas dengan air berlari.</p>
+<h3><span style= "warna: #33966;" >Masker Wajah persik<br / >
+</span></h3>
+<p>tekan jus dari satu buah persik besar dan campur dengan satu tbsp. bubur gandum, bagian yang sama dengan minyak alpukat, komponennya sangat bagus, ... campurannya didistribusikan pada permukaan masalah, melewati wilayah sekitar mata, setelah 15 menit. cuci muka dengan hangat, lalu air dingin, menerapkan tonik pelembab. Prosedur harus dilakukan dua kali seminggu, selama tiga sampai empat minggu, sampai hasil yang positif tercapai hasilnya.</p>
+<p>2. Gosokkan daging satu atau dua buah persik dengan garpu sampai halus, tambahkan satu tbsp. madu bunga, menerapkan campuran kulit dengan lapisan Murah hati, menunggu 25 menit sampai vitamin dan mineral diserap, kemudian menghapus sisa-sisa dengan spons basah, dan menerapkan krim bergizi.</p>
+<p>3. Peach puree menggabungkan setengah pisang dengan bubur, menerapkan isi ke kulit, menunggu 20-25 menit sampai nutrisi diserap, dan kemudian bilas.</p>
+<h3><gaya span= "color: # 33966;" >Moisturizing peach face mask</span></h3>
+<p>kupas persik ukuran sedang, tepatkan ke parutan, atau tumbuk dengan garpu sampai menjadi bubur, campurkan dengan satu tabloid krim asam medium-gemuk, terapkan campuran ke wajah, leher, dan decolete daerah, tunggu selama 2530 menit, kemudian mencuci sisa masker dengan air, dan menerapkan pelembab.</p>
+<h3><span style= "color: #33966;" >kosmetik es untuk jus peach<br / >
+</span></h3>
+<p>jika Anda ingin nada dan menyegarkan kulit, Anda dapat mempersiapkan kosmetik jus persik es. Genggam jus dari buah pulp, tuangkan ke dalam cetakan es khusus, dan masukkan ke dalam freezer selama 7-9 jam. Gunakan sebagai dimaksudkan, sekali atau dua kali sehari.</p>
+<h3><span style= "warna: #33966;" >wajah Peach scrub</span></h3>
+<p>puree Persik dibuat dari satu buah, campuran dengan satu C. l. oatmeal, Mari berdiri selama lima menit.  Terapkan semak belukar di arah garis kulit dengan gerakan pijat, pergi selama 7 menit.  Cuci wajahmu dengan air suhu kamar.</p>
+<p>sekarang anda tahu bagaimana membuat masker wajah persik, dan Anda dapat menggunakan resep yang sesuai untuk jenis kulit anda.</p>
+<p>anda juga dapat mengetahui lebih lanjut tentang sebuah artikel di blog saya tentang kecantikan dan kesehatan https://vseokrasote.com:</p>
+<p>menghadapi masker wajah setelah 50 tahun, yang menyediakan banyak resep efektif untuk matang, kulit pucat, yang dapat disiapkan di rumah.</p>
+<h3><gaya span= "color: # 33966;" > Ulasan<br / >
+</span></h3>
+<p>Lyudmila, 43 tahun.</p>
+<p>aku punya kombinasi kulit wajah. Saya mencampur persik pulp dengan krim asam, menambahkan sedikit jus lemon, menerapkan masker di kulit dua atau tiga kali seminggu. Aku terlihat jauh lebih baik daripada sebelum prosedur anti-penuaan.</p>
+<p>Eugene, 45 tahun.</p>
+<p>penuaan & # 8212;proses yang perlu diperjuangkan setiap hari). Aku memakai topeng dari sayuran dan buah-buahan, termasuk persik. Masih digunakan krim bergizi tinggi.</p>
+<p>Nadezhda, usia 57 tahun.</p>
+<p>setelah 50 tahun, kulit mulai usia lebih cepat. Saya menggunakan resep untuk kekeringan. Campur cheee persik dengan satu tsp madu dan kuning telur, menerapkan campuran dua atau tiga kali seminggu.</p>
+
+					</div></p>
+
+        </div><!-- post-top-area -->
+
+        <div class="post-bottom-area">
+
+            <div class="post-icons-area">
+                <ul class="post-icons">
+                    <li><a href="#"><i class="ion-heart"></i><?=$likes?></a></li>
+                    <li><a href="#"><i class="ion-chatbubble"></i>0</a></li>
+                    <li><a href="#"><i class="ion-eye"></i><?=$views?></a></li>
+                </ul>
+
+                <ul class="icons">
+                    <li>SHARE : </li>
+                    <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                    <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                    <li><a href="#"><i class="ion-social-pinterest"></i></a></li>
+                </ul>
+            </div>
+
+            <div class="post-footer post-info">
+
+                <div class="left-area">
+                    <a class="avatar" href="#"><img src="img/background/0843-Oa-ahaABi6w.jpg" alt="Blog tentang kecantikan dan kesehatan!"></a>
+                </div>
+
+                <div class="middle-area">
+                    <a class="name" href="#"><b>tentang keindahan</b></a>
+                    <h6 class="date"><?=$date?></h6>
+                </div>
+
+            </div><!-- post-info -->
+
+        </div><!-- post-bottom-area -->
+
+    </div><!-- main-post -->
+</div><!-- col-lg-8 col-md-12 -->
+
+<script>
+    $(function() {
+        $('#title').html($('.post-top-area strong').html());
+    });
+</script>
---
+++ main.php
@@ -0,0 +1,138 @@
+<?php
+$date = date('d-m-Y', time()-rand(13324, 1622211));
+$likes = rand(0, 100);
+$views = rand(0, 1000);
+?>
+
+<div class="col-lg-1 col-md-0"></div>
+<div class="col-lg-10 col-md-12">
+
+    <div class="main-post">
+
+        <div class="post-top-area">
+
+            <h5 class="pre-title">Blog tentang kecantikan dan kesehatan!</h5>
+
+            <h3 class="title"><a href="#"><b id="title"></b></a></h3>
+
+            <div class="post-info">
+
+                <div class="left-area">
+                    <a class="avatar" href="#"><img src="img/background/0843-Oa-ahaABi6w.jpg" alt="Blog tentang kecantikan dan kesehatan!"></a>
+                </div>
+
+                <div class="middle-area">
+                    <a class="name" href="#"><b>tentang keindahan</b></a>
+                    <h6 class="date"><?=$date?></h6>
+                </div>
+
+            </div><!-- post-info -->
+            <br>
+            <img class="w-100" src="./img/themes/healthandbeauty/jakob-owens-lwTzL-QwhQA-unsplash.jpg" alt="">
+            <p class="para"> subjek <strong>: Sorrel untuk wajah. Masker wajah berbulu </strong>
+
+				<div class="entry-content">
+			<p>di alam, ada banyak tanaman yang dapat digunakan sebagai bahan kosmetik untuk peremajaan kulit. Hari ini kita akan bicara tentang manfaat sorrel untuk wajah. Banyak wanita menggunakan tanaman yang menakjubkan ini untuk memasak kesenangan kuliner. Tapi apakah mereka tahu cara mempersiapkan masker wajah dari sorrel? Agen anti penuaan ini bisa digunakan di rumah.</p>
+<h2><gaya span= "color: # 33966;" >Sorrel untuk kulit wajah: gunakan dalam kosmetologi<br / >
+</span></h2>
+<p>Sorrel menemukan penggunaannya dalam kosmetologi berabad-abad yang lalu. Itu digunakan untuk bertarung melawan edema, bintik-bintik, berombak bulu dan ketulisan kulit. Ini mengandung vitamin dan mineral yang diperlukan untuk mempertahankan kecantikan wanita. Atas dasar tanaman, Pemutihan, pelembab, dan masker anti-penuaan siap. Mereka memiliki beberapa keuntungan atas kosmetik yang dibeli, karena mereka tidak mengandung bahan berbahaya aditif.</p>
+ topeng asli <p>Oxalis dalam bentuk asli mereka direkomendasikan oleh pemilik kulit berminyak. Jika dikombinasikan dengan bahan-bahan lain, cocok untuk semua jenis kulit, dan tidak memiliki pembatasan usia. Mereka kaya raya asam oxalic, flavonoid, tanin, vitamin C, PP, B1, B2, K. berkat yang berguna komposisi, mungkin untuk memperbaiki banyak kekurangan kosmetik, yaitu:</p>
+<UI>
+<li>bercak hitam putih.</li>
+<li>kurangi jumlah dan kedalaman keriput.</li>
+<li>kencangkan sabuk segel kendur wajah.</li>
+<li>kurangi kelezatan kulit.</li>
+<li>singkirkan jerawat, jerawat, jerawat.</li>
+</ul>
+<h2><gaya span= "color: #33966;" >masker wajah Terrel: yang terbaik resep</span></h2>
+<p>ini adalah beberapa resep yang berguna yang dapat digunakan sebagai agen anti-penuaan.</p>
+<h3><span style= "color: #33966;" >Mask untuk kulit kering <br / >
+</span></h3>
+<p>komposisi direkomendasikan untuk pelembab kering,kulit ikal. Potongan rambut dan menggiling, bercampur dengan yogurt dalam rasio 1:2, Tambahkan satu Hisp minyak zaitun, campur semuanya dengan baik, mendistribusikan di dermis, mencuci dengan air hangat setelah 25 menit.</p>
+<h3><span style= "color: #33966;" >Mask untuk kulit normal <br / >
+</span></h3>
+<p>produk akan memperkaya kulit dengan vitamin dan mineral, meningkatkan nada suaranya, dan menghilangkan keriput. Satu porbsp halus tanah sorrel dicampur dengan kuning telur kocok, campuran diterapkan untuk dermis, setelah 20 menit, mencuci dengan air yang mengalir.</p>
+<h3><span style= "color: #33966;" >Mask untuk kulit berminyak <br / >
+</span></h3>
+<p>komposisi disarankan untuk menerapkan ke kulit berminyak untuk memerangi keriput, jerawat, dan bintik-bintik umur. Sorrel (50 gr. potongan halus, tuang lemon juice (50 ml.), tambahkan dicambuk putih telur, campur semuanya, menerapkan campuran untuk kulit, mencuci dengan air hangat setelah 25 menit.</p>
+<h3><span style= "color: #33966;" >Masker Wajah untuk kulit kombinasi<br / >
+</span></h3>
+<p>produk tersebut direkomendasikan untuk gizi dan peremajaan dari kulit gabungan. Potongan rambut berbulu dengan pisau, mengambil dua sendok tabloid dari bahan, menambahkan jumlah yang sama pondok buatan sendiri keju dan yogurt, semua baik aduk, mendistribusikan pada dermis, cuti untuk merendam selama 20 menit, kemudian bilas dengan air berjalan pada suhu kamar.</p>
+<h3><span style= "color: # 33966;" > Mask untuk semua jenis kulit </span></h3>
+<p>Produk disarankan untuk diterapkan pada jenis kulit yang berbeda. Ini nutrisi, pelembab, nada, dan jenuh substansi yang berguna. Hal ini diperlukan untuk menggabungkan satu sendok makan daun sorrel yang teriris, warna hitam dan Rowan, tambahkan dua sendok krim asam buatan sendiri, campuran bahan-bahan nah, mendistribusikan campuran pada epidermis epidermis, mencuci dengan suhu air di kamar setelah 20 menit.</p>
+<h3><gaya span= "color: #33966;" >a Lotion made from sorrel for wajah</span></h3>
+<p>untuk mempersiapkan lotion, Anda akan membutuhkan: crushed sorrel,bunga chamomile, pisang, kalender, (Semua satu tbsp.). tuangkan campuran Herbal ke dalam botol kaca, tuangkan cognac (100 gr.), taruh ke infus di tempat dingin yang gelap. Simpan produk kosmetik yang sudah selesai di kulkas. Gunakan setiap hari untuk menyeka kulit.</p>
+<h3><gaya span= "warna: # 33966;" >sorrel broth es batu</span></h3>
+<p>untuk mempersiapkan es batu dari kaldu sorrel, Anda perlu mengambil satu tbsp. kemudian tuangkanlah air yang sangat panas dalam 20 menit. kemudian tuangkanlah dalam bentuk jala yang sangat sempurna. freezer untuk beku. Setelah 7-8 jam, es akan siap. Gunakan es batu untuk menggosok wajah Anda sehari-hari di arah garis pijat. Prosedur harus dilakukan dengan hati-hati agar tidak merusak kulit.</p>
+<h3><span style= "color: # 33966;" >acne Mask</span></h3>
+<p>alat membantu untuk menyingkirkan jerawat. Sorrel finely potong dan menggiling, satu tbsp bahan menuangkan sedikit jus lemon, campuran segalanya, mendistribusikan campuran pada dermis, termasuk kontak dengan mata, setelah 15 menit, bilas dengan air yang mengalir.</p>
+<h3><span style= "color: # 33966;" >anti-wrinkle Mask</span></h3>
+<p>Resep membantu melawan keriput, sehingga warna kulit berubah. Itis chop the sorrel dan parsley, ambil satu tbsp setiap bahan, tambahkan satu tarragon, campurkan semuanya, tuangkan segelas susu panas, biarkan berdiri selama 15 menit, kemudian strain. Tambahkan terigu dalam rasio 1: 2, campur lagi, mendistribusikan campuran pada kulit, menghapusnya setelah 25 menit dengan air yang berjalan waters.</p>
+<h3><span style= "color: #33966;" >pigment spot Mask</span></h3>
+<p>Resep menghilangkan pigmentasi dan keriput. Terdiri dari daun elder, sorrel dan bayam. Semua komponen dipotong dengan pisau, ambil 20 gr. masing-masing, tambahkan sedikit strawberry puree, campurkan semuanya dengan baik, tuangkan sedikit kefir mendapatkan massa homogen. Menerapkan campuran merata ke kulit, meninggalkan untuk 10-15 menit, kemudian bilas dengan air berjalan.</p>
+<h3><span style= "color: #33966;" >Whitening mask<br / >
+</span></h3>
+<p>komposisi memutihkan kulit, membuat titik-titik babi kurang dapat difermentasi. Satu tabloid tanah liat putih tuangkan sedikit jus menghubungkan semuanya, mendistribusikan pada epidermis, mencuci dengan air yang mengalir dalam 20 menit.</p>
+<h3><gaya span= "color: # 33966;" >Moisturizing mask<br / >
+</span></h3>
+<p>Produk menormalkan keseimbangan air dan melindungi terhadap paparan matahari. Tekan 7 ml. jus sorrel, campur dengan jumlah yang sama minyak biji-bijian, menerapkan campuran untuk dermis, setelah 20 cuci dengan air hangat selama beberapa menit, lalu dengan air dingin.</p>
+<h3><gaya = "warna: # 33966;" >masker anti-penuaan<br / >
+</span></h3>
+<p>komposisi bergizi, pelembab, perkelahian, meningkatkan warna kulit. Gabungkan satu tbsp dari sorrel hancur dengan jumlah sama yogurt, tambahkan 20 tetes vitamin E, campur semuanya dengan baik, dan sebarkan pada biarkan kulit menyerap selama 20 menit, kemudian mencuci dengan hangat, kemudian air dingin, dan menerapkan krim bergizi.</p>
+<h3><span style= "color: #33966;" >eye Mask</span></h3>
+<p>komposisi membantu untuk menyingkirkan puffiness dan Keriput Di bawah mata. Potongan halus dan menggiling, mengambil satu puncak bahan, menggabungkan dengan satu tbsp. krim asam buatan sendiri, mencampur segalanya dengan baik, dengan lembut menerapkannya pada kulit di sekitar mata, meninggalkan menyerap selama 10 menit, setelah waktu berlalu, bilas dengan air berlari.</p>
+<h3><span style= "color: #33966;" >blackhead Mask</span></h3>
+<p>komposisi memiliki efek pembersihan dan anti-peradangan. Hal ini diperlukan untuk mengambil satu sendok makan dari tanah halus sorrel dan bunga chamomile kering, campuran bahan-bahan, tuangkan campuran dengan segelas air mendidih, dimasukkan ke api, didihkan dalam mandi air selama 5-7 menit, kemudian dingin, ketegangan. Terapkan bubur yang tersisa untuk kulit, tahan selama 10 menit, kemudian kocok dengan serbet, dan mencuci dengan kaldu yang dihasilkan. Lakukan saja seperti prosedur, sampai hasil yang diinginkan diperoleh.</p>
+<h3><span style= "color: # 33966;" >Contraclations</span></h3>
+<p>ketika menerapkan masker oxalic, itu perlu untuk mengambil ke dalam ketentuan account. Include ini:</p>
+<UI>
+<li>Individual intoleransi terhadap pabrik.</li>
+<li>bukan penyembuhan luka, retak.</li>
+<li>proses inflamasi parah.</li>
+</ul>
+<p>sekarang kau tahu, cara membuat masker wajah sorrel dan menggunakan resep terbaik yang sesuai jenis kulit anda.</p>
+
+					</div></p>
+
+        </div><!-- post-top-area -->
+
+        <div class="post-bottom-area">
+
+            <div class="post-icons-area">
+                <ul class="post-icons">
+                    <li><a href="#"><i class="ion-heart"></i><?=$likes?></a></li>
+                    <li><a href="#"><i class="ion-chatbubble"></i>0</a></li>
+                    <li><a href="#"><i class="ion-eye"></i><?=$views?></a></li>
+                </ul>
+
+                <ul class="icons">
+                    <li>SHARE : </li>
+                    <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                    <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                    <li><a href="#"><i class="ion-social-pinterest"></i></a></li>
+                </ul>
+            </div>
+
+            <div class="post-footer post-info">
+
+                <div class="left-area">
+                    <a class="avatar" href="#"><img src="img/background/0843-Oa-ahaABi6w.jpg" alt="Blog tentang kecantikan dan kesehatan!"></a>
+                </div>
+
+                <div class="middle-area">
+                    <a class="name" href="#"><b>tentang keindahan</b></a>
+                    <h6 class="date"><?=$date?></h6>
+                </div>
+
+            </div><!-- post-info -->
+
+        </div><!-- post-bottom-area -->
+
+    </div><!-- main-post -->
+</div><!-- col-lg-8 col-md-12 -->
+<script>
+    $(function() {
+       $('#title').html($('.post-top-area strong').html());
+    });
+</script>
+
